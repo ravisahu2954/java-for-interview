8b62c69ced47fd1c1e34ba095d8208eaffe6ce39
ravisahu2954 <82141425+ravisahu2954@users.noreply.github.com>
2021-08-13 04:51:13 -0700

    Update Insertsymbol.java

--- Java/Insertsymbol.java
+++ Java/Insertsymbol.java
@@ -19,3 +19,25 @@ public class String_s {
 }
 input:ravi
 output: r*a**v***i****
+
+
+//***************//
+public class String_s {
+
+	public static void main(String[] args) {
+	
+		String name="ravi";
+		String ne="";
+		for(int i=0;i<name.length();i++)
+		{
+			ne+=name.charAt(i)+"*";
+	        }
+		System.out.print(ne);
+	
+	}
+	
+
+}
+input:ravi
+output: r*a*v*i*
+
