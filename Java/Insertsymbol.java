2980a5bb019029e5ff1ba1218dd29e6c03797ed1
ravisahu2954 <82141425+ravisahu2954@users.noreply.github.com>
2021-08-13 04:47:14 -0700

    Create Insertsymbol.java

---
+++ Java/Insertsymbol.java
@@ -0,0 +1,21 @@
+public class String_s {
+
+	public static void main(String[] args) {
+	
+		String name="ravi";
+		String ne="";
+		for(int i=0;i<name.length();i++)
+		{
+			ne+=name.charAt(i);
+			for(int j=0;j<=i;j++)
+			{
+			//	name = name.substring(0, i+1)+ "*" +name.substring(i+1);
+			    ne+="*";
+			}
+		}
+		System.out.print(ne);
+	
+	}
+}
+input:ravi
+output: r*a**v***i****
