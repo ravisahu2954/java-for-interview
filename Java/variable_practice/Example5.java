3c7429f3678b6bbef1c889db2c4b87ca3ffb7535
ravisahu2954 <82141425+ravisahu2954@users.noreply.github.com>
2021-08-21 04:53:17 -0700

    Update Example5.java

--- Java/variable_practice/Example5.java
+++ Java/variable_practice/Example5.java
@@ -5,22 +5,23 @@ public class Example5 {
 
 	public static void main(String[] args) {
 		
-		int f=10;
+		 int f=10;
 		double t=5.6;
-		Integer pt = Integer.valueOf(f);
-		Integer st = new Integer(5);
-		System.out.println(pt+st);
+		Double bOb = Double.valueOf(t); //bob=5.6
+		Integer pt = Integer.valueOf(f);// pt =10
+		Integer st = new Integer(5);    //st=5
+		System.out.println(pt+bOb+" "+pt+st); // pt+bob=15.6
 		
 		 // creates objects of wrapper class
-	    Integer aObj = Integer.valueOf(23);
-	    Double bObj = Double.valueOf(5.55);
+	        Integer aObj = Integer.valueOf(23);
+	        Double bObj = Double.valueOf(5.55);
 
-	    // converts into primitive types
-	    int a = aObj.intValue();
-	    double b = bObj.doubleValue();
+	        // converts into primitive types
+	        int a = aObj.intValue();
+	        double b = bObj.doubleValue();
 
-	    System.out.println("The value of a: " + a);
-	    System.out.println("The value of b: " + b);
+	       System.out.println("The value of a: " + a);
+	       System.out.println("The value of b: " + b);
 		
 
 	}
