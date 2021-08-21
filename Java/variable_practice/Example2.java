d681e17589f04a44b975138b5488d124cd50500a
ravisahu2954 <82141425+ravisahu2954@users.noreply.github.com>
2021-08-21 04:40:16 -0700

    Update Example2.java

--- Java/variable_practice/Example2.java
+++ Java/variable_practice/Example2.java
@@ -4,18 +4,19 @@ package variable_practice;
 public class Example2 {
 
 	public static void main(String[] args) {
-		Integer obj = 10;
+		 
+                Integer obj = 10;
 		int sc=10;
 		if(sc==obj)
-		  System.out.print("same");  //output=same
+		  System.out.println("same");  //output=same
 		else
-			System.out.print("not same");
+			System.out.println("not same");
 		
 		Integer ob=10;
 		if(ob==obj)
-			  System.out.print("same");  
+			  System.out.println("same");//output=same  
 		else
-		         System.out.print("not same");//output= not same(because object are diffrent )
+		    System.out.println("not same");
 			
 		
 		class X{ int t;}
