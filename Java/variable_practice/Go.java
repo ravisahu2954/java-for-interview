bbb91dcd6293d7b9d6d6f5ecde5ced8c1788443e
ravisahu2954 <82141425+ravisahu2954@users.noreply.github.com>
2021-10-26 19:59:28 +0530

    Create Go.java

---
+++ Java/variable_practice/Go.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public static void main(String args[]) {
+     	int a1=56;
+        char ch=(char)a1;
+        System.out.println(ch);
+    }
+}
+
+output :8 //56 is ascii code of 8
